3aede2bc4233735c6db2b33b851b50e5ad1c7512
Xyunn <maxime.devecchi@gmail.com>
2017-02-27 14:01:59 +0100

    Beginning for the shells steps

--- Tank_Tutorial/Assets/Scripts/Shell/ShellExplosion.cs
+++ Tank_Tutorial/Assets/Scripts/Shell/ShellExplosion.cs
@@ -20,6 +20,21 @@ public class ShellExplosion : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         // Find all the tanks in an area around the shell and damage them.
+        Collider[] colliders = Physics.OverlapSphere(transform.position, m_ExplosionRadius, m_TankMask);
+
+        for (int i = 0; i < colliders.Length; ++i)
+        {
+            Rigidbody targetRigidBody = colliders[i].GetComponent<Rigidbody>();
+
+            if (!targetRigidBody)
+            {
+                continue;
+            }
+
+            targetRigidBody.AddExplosionForce(m_ExplosionForce, transform.position, m_ExplosionRadius);
+
+            //TankHealth targetHealth = 
+        }
     }
 
 
